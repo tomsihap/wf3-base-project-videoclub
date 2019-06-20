1ba41447d482513911fa187553f5b2e3fa28efbb
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2019-06-20 10:19:29 +0200

    Fixes

--- config/config.php
+++ config/config.php
@@ -7,13 +7,13 @@
 ini_set('allow_url_include', 1);
 
 const DB_HOST = 'localhost';
-const DB_PORT = '3308';
-const DB_NAME = 'videoclub';
+const DB_PORT = '3306';
+const DB_NAME = '';
 const DB_USER = 'root';
 const DB_PWD  = '';
 
-const WEBSITE_TITLE = "Mon nouveau site";
-const BASE_URL = "localhost/videoclub";
+const WEBSITE_TITLE = "";
+const BASE_URL = "";
 
 const CLASSES_SOURCES = [
     'src/controller',
